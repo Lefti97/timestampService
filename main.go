a589e0082090a7a0d17054243f08dcc1a67d0313
Lefti <vlefti@hotmail.com>
2022-12-23 21:12:08 +0200

    server ready

--- main.go
+++ main.go
@@ -7,34 +7,39 @@ import (
 	"io"
 	"net"
 	"net/http"
+	"os"
 )
 
 const keyServerAddr = "serverAddr"
 
 func getPtList(w http.ResponseWriter, r *http.Request) {
-	//ctx := r.Context()
-
-	//hasPeriod := r.URL.Query().Has("period")
 	period := r.URL.Query().Get("period")
-	//hasTz := r.URL.Query().Has("tz")
 	tz := r.URL.Query().Get("tz")
-	//hasT1 := r.URL.Query().Has("t1")
 	t1 := r.URL.Query().Get("t1")
-	//hasT2 := r.URL.Query().Has("t2")
 	t2 := r.URL.Query().Get("t2")
 
-	fmt.Printf("got / request. period=%s, tz=%s, t1=%s, t2=%s\n",
-		period, tz, t1, t2)
+	// call function to create json array response
+
+	fmt.Printf("%s %s\n", r.Method, r.URL.JoinPath())
 	io.WriteString(w, "period="+period+", tz="+tz+", t1="+t1+", t2="+t2+"\n")
 }
 
 func main() {
+	portNum := ""
+
+	if len(os.Args) >= 2 {
+		portNum = os.Args[1]
+	} else {
+		fmt.Printf("Port number must be given as command line argument.\n")
+		os.Exit(1)
+	}
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/ptlist", getPtList)
 
 	ctx := context.Background()
 	server := &http.Server{
-		Addr:    ":3333",
+		Addr:    ":" + portNum,
 		Handler: mux,
 		BaseContext: func(l net.Listener) context.Context {
 			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
